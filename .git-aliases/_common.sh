0d1da67ff0afe726975c09421ab871f26d54d09c
EvitanRelta <shauntanzongzhi@gmail.com>
2023-10-01 23:21:27 +0800

    Move scripts into named dir

--- .git-aliases/_common.sh
+++ .git-aliases/_common.sh
@@ -4,13 +4,13 @@
 cd "${GIT_PREFIX:-.}"
 
 # Get the name of the alias from the calling script's name
-alias_name=$(basename "$0" | sed 's/\.sh$//')
+alias_name=$(basename "$(dirname "$0")")
 
 # Check if the --docs flag is provided in any position
 for arg in "$@"; do
     if [ "$arg" = "--docs" ]; then
         # Display the documentation for the alias
-        cat ~/.git-aliases/docs/${alias_name}.md
+        cat ~/.git-aliases/${alias_name}/docs.md
         exit 0
     fi
 done
