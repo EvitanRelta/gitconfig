d270f2a1ee9c825ae7c37c090b231e520e98e7f3
EvitanRelta <shauntanzongzhi@gmail.com>
2023-10-13 16:16:59 +0800

    Set common script paths to be relative

--- .git-aliases/_common.sh
+++ .git-aliases/_common.sh
@@ -3,14 +3,11 @@
 # Change to the GIT_PREFIX directory or current directory if GIT_PREFIX is not set
 cd "${GIT_PREFIX:-.}"
 
-# Get the name of the alias from the calling script's name
-alias_name=$(basename "$(dirname "$0")")
-
 # Check if the --docs flag is provided in any position
 for arg in "$@"; do
     if [ "$arg" = "--docs" ]; then
         # Display the documentation for the alias
-        cat ~/.git-aliases/${alias_name}/docs.md
+        cat "$(dirname "$0")/docs.md"
         exit 0
     fi
 done
