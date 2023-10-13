a8d9080e699c2b3bdca4c596e89238f01437cd0b
EvitanRelta <shauntanzongzhi@gmail.com>
2023-10-13 17:33:41 +0800

    Simplify 'restore-deleted-branch' code

--- .git-aliases/.helper_functions/restore-deleted-branch/script.sh
+++ .git-aliases/.helper_functions/restore-deleted-branch/script.sh
@@ -4,8 +4,7 @@
 if [ $# -eq 1 ]; then
     commit_subject=$(git log --format=%s -n 1 "$1") &&
         branch_name=$(grep -Po "(?<=')[^']+(?=')" <<< "$commit_subject" | head -1) &&
-        git checkout -b "$branch_name" "$1^2" &&
-        git checkout -
+        git branch "$branch_name" "$1^2"
 else
     echo Invalid parameters
 fi
