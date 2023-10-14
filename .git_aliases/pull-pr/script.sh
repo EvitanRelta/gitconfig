34304feda68885dda254ef84e53b225f91f2ac6a
EvitanRelta <shauntanzongzhi@gmail.com>
2023-10-14 13:33:52 +0800

    Add 'pull-pr' alias

---
+++ .git_aliases/pull-pr/script.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+aliases_root_dir="$(dirname "$0")/.."
+source "$aliases_root_dir/.common.sh"
+#!/bin/bash
+
+# Check if the script received the necessary input
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 <username:branch> [new_remote_name]"
+    exit 1
+fi
+
+# Split input into username and branch
+IFS=':' read -ra ADDR <<< "$1"
+USERNAME=${ADDR[0]}
+BRANCH=${ADDR[1]}
+NEW_REMOTE_NAME=${2:-$USERNAME}
+
+# Search the remote name with the URL containing the username
+REMOTE_NAME=$(git remote -v | grep "$USERNAME" | awk '{print $1}' | uniq)
+
+# Check if the remote name is found
+if [ -z "$REMOTE_NAME" ]; then
+    echo "No remote found with URL containing '$USERNAME'"
+    
+    # Extract the URL of the 'origin' remote
+    ORIGIN_URL=$(git remote get-url origin)
+    
+    # Extract repository from the `origin` URL
+    REPO=$(echo "$ORIGIN_URL" | awk -F/ '{print $NF}')
+
+    # Construct new URL
+    NEW_URL="https://github.com/$USERNAME/$REPO"
+    
+    # Add a new remote with the new URL
+    git remote add "$NEW_REMOTE_NAME" "$NEW_URL"
+    REMOTE_NAME="$NEW_REMOTE_NAME"
+
+# If new remote name (ie. $2) is given
+elif [ -n "$2" ]; then
+    # And found remote name doesn't equal to the new provided name, rename it
+    if [ "$REMOTE_NAME" != "$NEW_REMOTE_NAME" ]; then
+        git remote rename "$REMOTE_NAME" "$NEW_REMOTE_NAME"
+        REMOTE_NAME="$NEW_REMOTE_NAME"
+    fi
+fi
+
+# Fetch from the identified/modified remote
+git fetch "$REMOTE_NAME"
+
+# Check if the branch exists in the remote
+if ! git show-ref --verify --quiet "refs/remotes/$REMOTE_NAME/$BRANCH"; then
+    echo "Branch '$BRANCH' not found in remote '$REMOTE_NAME'"
+    exit 1
+fi
+
+# Create and checkout to a new branch, set to track the remote branch
+git checkout -b "$REMOTE_NAME-$BRANCH" "$REMOTE_NAME/$BRANCH"
+
+# Output the current branch
+echo "Switched to branch '$REMOTE_NAME-$BRANCH'"
--- README.md
+++ README.md
@@ -281,6 +281,58 @@ git reset --hard "origin/$(git branch --show-current)"
 
 <br>
 
+### `git pull-pr [username]:[branch] [optional_remote_name]`
+
+Pulls (and checkout to) a PR's branch. The `[username]:[branch]` is given in
+the GitHub's PR description.
+
+If no remote with a URL containing `[username]` exists, create a new remote
+named `[username]` (or `[optional_remote_name]` if given) with a URL based on
+`origin` remote. \
+_(eg. If `origin  https://github.com/EvitanRelta/repo` then the URL will be `https://github.com/[username]/repo`)_
+
+If a remote exists, but it's name isn't equal to `[optional_remote_name]`, then
+that remote is renamed to `[optional_remote_name]`.
+
+For example, for the PR description: \
+"afiqzu wants to merge 1 commit into `CS3219-AY2324S1:assignment-5` from `afiqzu:assignment-5`" \
+`[username]:[branch]` = `afiqzu:assignment-5`
+
+```bash
+# Given this scenario:
+$ git remote -v
+oldremote  https://github.com/EvitanRelta/repo
+origin     https://github.com/myorigin/repo
+```
+
+#### Example usage 1:
+```bash
+git pull-pr EvitanRelta:pr-branch
+# is alias for:
+git fetch oldremote
+git checkout -b oldremote-pr-branch oldremote/pr-branch
+```
+
+#### Example usage 2:
+```bash
+git pull-pr EvitanRelta:pr-branch newremote
+# is alias for:
+git remote rename oldremote newremote
+git fetch newremote
+git checkout -b newremote-pr-branch newremote/pr-branch
+```
+
+#### Example usage 3:
+```bash
+git pull-pr NewUser:pr-branch newremote
+# is alias for:
+git remote add newremote https://github.com/NewUser/repo
+git fetch newremote
+git checkout -b newremote-pr-branch newremote/pr-branch
+```
+
+<br>
+
 ### `git re [flags/parameters]`
 
 Alias for rebase.
