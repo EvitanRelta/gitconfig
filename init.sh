8f2023ea57758f56e7d810b18a9b73821df9291f
EvitanRelta <shauntanzongzhi@gmail.com>
2023-10-25 21:10:58 +0800

    Add script for installing the configs into system

---
+++ init.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Define an array of the files and directories to check
+files_and_dirs=(.bash_profile .bashrc .git_aliases/ .gitconfig .vim/ .vimrc)
+
+# Array to store existing files and directories
+exists=()
+
+# Check if each file or directory exists in the root directory
+for item in "${files_and_dirs[@]}"; do
+    if [[ -e ~/"$item" ]]; then
+        exists+=("$item")
+    fi
+done
+
+# If any file or directory exists, prompt the user for confirmation
+if [[ ${#exists[@]} -ne 0 ]]; then
+    echo "These files/directories will be replaced: ${exists[@]}"
+    read -p "Do you want to continue? (yes/no): " response
+    if [[ "$response" != "yes" ]]; then
+        echo "Operation aborted."
+        exit 1
+    fi
+fi
+
+# Remove any existing files/directories or symlinks from the user's home directory
+for item in "${exists[@]}"; do
+    # Check without trailing /
+    base_item="${item%/}"
+
+    if [[ -L ~/"$base_item" ]]; then
+        # It's a symlink, remove it
+        rm ~/"$base_item"
+    elif [[ -e ~/"$base_item" ]]; then
+        # It's an actual file or directory
+        rm -rf ~/"$base_item"
+    fi
+done
+
+# Create symlinks from the script's current directory to the user's home directory
+for item in "${files_and_dirs[@]}"; do
+    base_item="${item%/}"
+    ln -s "$DIR/$base_item" ~/"$base_item"
+done
+
+echo "Symlinks created successfully."
+
+# Initialise `vim-colorschemes` submodule.
+cd "$DIR"
+git submodule update --init
