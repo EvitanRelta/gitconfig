e697ea883e46097b66f802f4a4e45ecb61cc7f6a
EvitanRelta <shauntanzongzhi@gmail.com>
2024-06-12 11:12:06 +0800

    Fix editor resolution for WSL

--- .git_aliases/.git_editor.sh
+++ .git_aliases/.git_editor.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-VSCODE_EDITOR_PATH="$LOCALAPPDATA\\Programs\\Microsoft VS Code\\bin\\code"
+# If in WSL, resolve the "$LOCALAPPDATA" variable and translate WSL paths to
+# regular Windows paths.
+if [[ -n "$WSL_DISTRO_NAME" ]]; then
+    LOCALAPPDATA=$(wslpath "$(cmd.exe /c "<nul set /p=%LOCALAPPDATA%" 2>/dev/null)")
+    set -- $(wslpath -w "$@")
+fi
+
+VSCODE_EDITOR_PATH="$LOCALAPPDATA/Programs/Microsoft VS Code/bin/code"
+
 
 # Use VSCode as editor if it exists, else use Vim.
 if command -v "$VSCODE_EDITOR_PATH" > /dev/null 2>&1; then
@@ -8,4 +16,3 @@ if command -v "$VSCODE_EDITOR_PATH" > /dev/null 2>&1; then
 else
     vim "$@"
 fi
-
