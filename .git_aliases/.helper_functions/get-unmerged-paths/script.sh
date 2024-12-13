2177574b0c4f4d107be35b7144503f4026123a7d
EvitanRelta <shauntanzongzhi@gmail.com>
2024-12-13 19:04:50 +0800

    Fix 'get-unmerge-paths' alias not working for Linux

--- .git_aliases/.helper_functions/get-unmerged-paths/script.sh
+++ .git_aliases/.helper_functions/get-unmerged-paths/script.sh
@@ -4,12 +4,15 @@ source "$aliases_root_dir/.common.sh"
 
 git_root=$(git rev-parse --show-toplevel)
 
-# Using `sed` to convert Linux-style path (eg. "/c/Users") to Unix-style (eg. "C:/Users").
-# This is because `git_root` is in Unix-style, and `realpath` only works when they match.
-current_dir=$(pwd | sed 's/^\/\([a-z]\)/\U\1:/')
+# Check if we're on Windows (Git Bash/MSYS)
+if [[ "$(uname -s)" == MINGW* ]] || [[ "$(uname -s)" == MSYS* ]]; then
+    # Using `sed` to convert Linux-style path (eg. "/c/Users") to Unix-style (eg. "C:/Users").
+    # This is because `git_root` is in Unix-style, and `realpath` only works when they match.
+    current_dir=$(pwd | sed 's/^\/\([a-z]\)/\U\1:\//')
+else
+    current_dir=$(pwd)
+fi
 
-# `git diff --name-only --diff-filter=U` gives paths relative to the repo root.
-# This converts them to be relative to the current dir.
 git diff --name-only --diff-filter=U | while read -r file; do
     realpath --relative-to="$current_dir" "$git_root/$file"
 done
