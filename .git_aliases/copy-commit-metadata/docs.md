446fc4039176e2313abb8d743e6ea3feb90032e6
EvitanRelta <shauntanzongzhi@gmail.com>
2025-02-15 08:56:17 +0800

    Add flags to 'copy-commit-metadata' to ctrl copied metadata

--- .git_aliases/copy-commit-metadata/docs.md
+++ .git_aliases/copy-commit-metadata/docs.md
@@ -1,19 +1,31 @@
-### `git copy-commit-metadata [commit_hash]`
+### `git copy-commit-metadata [--all|--committer|--author|--committer-date|--author-date|--message] [COMMIT_HASH]`
 
-Amends the last commit to copy the committer/author names and dates of `[commit_hash]`.
+Amends the last commit to copy the committer/author names and dates and commit
+message of `[COMMIT_HASH]`.
+
+Takes the flags which selects which metadata to copy:
+
+-   `--all`: All metadata
+-   `--committer`: Committer name/email
+-   `--author`: Author name/email
+-   `--committer-date`: Committer date
+-   `--author-date`: Author date
+-   `--message`: Commit message
 
 ```bash
 # Alias for:
-committer_name=$(git show -s --format='%cn' "$1")
-committer_email=$(git show -s --format='%ce' "$1")
-committer_date=$(git show -s --format='%cd' "$1")
-author=$(git show -s --format='%an <%ae>' "$1")
-author_date=$(git show -s --format='%ad' "$1")
+committer_name=$(git show -s --format='%cn' [COMMIT_HASH])
+committer_email=$(git show -s --format='%ce' [COMMIT_HASH])
+committer_date=$(git show -s --format='%cd' [COMMIT_HASH])
+author=$(git show -s --format='%an <%ae>' [COMMIT_HASH])
+author_date=$(git show -s --format='%ad' [COMMIT_HASH])
+commit_message=$(git show -s --format='%B' [COMMIT_HASH])
 
 GIT_COMMITTER_NAME="$committer_name" \
 GIT_COMMITTER_EMAIL="$committer_email" \
 GIT_COMMITTER_DATE="$committer_date" \
 git commit --amend --no-edit \
     --author="$author" \
-    --date="$author_date"
+    --date="$author_date" \
+    -m "$commit_message"
 ```
--- .git_aliases/copy-commit-metadata/script.sh
+++ .git_aliases/copy-commit-metadata/script.sh
@@ -1,16 +1,123 @@
 #!/bin/bash
 aliases_root_dir="$(dirname "$0")/.."
 source "$aliases_root_dir/.common.sh"
+set -e
 
-committer_name=$(git show -s --format='%cn' "$1")
-committer_email=$(git show -s --format='%ce' "$1")
-committer_date=$(git show -s --format='%cd' "$1")
-author=$(git show -s --format='%an <%ae>' "$1")
-author_date=$(git show -s --format='%ad' "$1")
+# Default: no flags are enabled
+copy_committer=false
+copy_author=false
+copy_committer_date=false
+copy_author_date=false
+copy_message=false
+copy_all=false
 
-GIT_COMMITTER_NAME="$committer_name" \
-GIT_COMMITTER_EMAIL="$committer_email" \
-GIT_COMMITTER_DATE="$committer_date" \
-git commit --amend --no-edit \
-    --author="$author" \
-    --date="$author_date"
+# Parse arguments
+commit_hash=""
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --all)
+            copy_all=true
+            shift
+            ;;
+        --committer)
+            copy_committer=true
+            shift
+            ;;
+        --author)
+            copy_author=true
+            shift
+            ;;
+        --committer-date)
+            copy_committer_date=true
+            shift
+            ;;
+        --author-date)
+            copy_author_date=true
+            shift
+            ;;
+        --message)
+            copy_message=true
+            shift
+            ;;
+        --*)
+            echo "Error: Unknown option $1"
+            echo "Valid options: --all, --committer, --author, --committer-date, --author-date, --message"
+            exit 1
+            ;;
+        *)
+            if [ -z "$commit_hash" ]; then
+                commit_hash="$1"
+                shift
+            else
+                echo "Error: Too many arguments. Only one commit hash is allowed."
+                exit 1
+            fi
+            ;;
+    esac
+done
+
+# Check if commit hash is provided
+if [ -z "$commit_hash" ]; then
+    echo "Error: Commit hash is required"
+    exit 1
+fi
+
+# Check if at least one option was selected
+if [ "$copy_committer" = false ] && [ "$copy_author" = false ] && 
+   [ "$copy_committer_date" = false ] && [ "$copy_author_date" = false ] && 
+   [ "$copy_message" = false ] && [ "$copy_all" = false ]; then
+    echo "Error: At least one option must be specified"
+    echo "Valid options: --all, --committer, --author, --committer-date, --author-date, --message"
+    exit 1
+fi
+
+
+# If --all is specified, enable all flags
+if [ "$copy_all" = true ]; then
+    copy_committer=true
+    copy_author=true
+    copy_committer_date=true
+    copy_author_date=true
+    copy_message=true
+fi
+
+# Initialize git command parameters
+git_env=""
+git_params="--amend --no-edit"
+
+# Get and apply committer info if requested
+if [ "$copy_committer" = true ]; then
+    committer_name=$(git show -s --format='%cn' "$commit_hash")
+    committer_email=$(git show -s --format='%ce' "$commit_hash")
+    git_env+="GIT_COMMITTER_NAME=\"$committer_name\" GIT_COMMITTER_EMAIL=\"$committer_email\" "
+fi
+    
+if [ "$copy_committer_date" = true ]; then
+    committer_date=$(git show -s --format='%cd' "$commit_hash")
+    git_env+="GIT_COMMITTER_DATE=\"$committer_date\" "
+fi
+
+# Get and apply author info if requested
+if [ "$copy_author" = true ]; then
+    author_name=$(git show -s --format='%an' "$commit_hash")
+    author_email=$(git show -s --format='%ae' "$commit_hash")
+    git_params+=" --author=\"$author_name <$author_email>\""
+fi
+
+# Get and apply author date if requested
+if [ "$copy_author_date" = true ]; then
+    author_date=$(git show -s --format='%ad' "$commit_hash")
+    git_params+=" --date=\"$author_date\""
+fi
+
+# Get and apply commit message if requested
+if [ "$copy_message" = true ]; then
+    commit_message=$(git show -s --format='%B' "$commit_hash")
+    git_params+=" -m \"$commit_message\""
+fi
+
+
+# Execute the git command
+eval $git_env git commit $git_params
+
+echo "Successfully copied selected information from commit $commit_hash"
